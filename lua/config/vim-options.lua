6455a2c27f918f0760d8158bc71f6c2bef02b8cb
Richmond Baltazar <116050858+Retchizu@users.noreply.github.com>
2025-05-05 21:59:19 +0800

    added bufferline plugin

--- lua/config/vim-options.lua
+++ lua/config/vim-options.lua
@@ -3,8 +3,6 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent selection" })
-vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Dedent selection" })
 vim.keymap.set("n", "<leader>t", ":belowright sp | terminal<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-Up>",    ":resize +2<CR>", { noremap = true, silent = true, desc = "Increase window height" })
 vim.keymap.set("n", "<C-Down>",  ":resize -2<CR>", { noremap = true, silent = true, desc = "Decrease window height" })
@@ -12,3 +10,5 @@ vim.keymap.set("n", "<C-Left>",  ":vertical resize -2<CR>", { noremap = true, si
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true, desc = "Increase window width" })
 vim.keymap.set("t", "<C-q>", "<C-\\><C-N>:q!<CR>", { noremap = true, silent = true, desc = "Close terminal" })
 vim.keymap.set("n", "<C-q>", ":bd!<CR>", { noremap = true, silent = true, desc = "Close terminal in normal mode" })
+vim.keymap.set("n", "<Tab>", ":bnext<CR>")
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>")
